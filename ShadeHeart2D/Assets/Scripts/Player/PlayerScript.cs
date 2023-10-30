56ad01276bdddff0ffc075c64fbc630475564584
Seth-Lopez <70123955+Seth-Lopez@users.noreply.github.com>
2023-10-30 12:45:55 -0700

    Added health and Energy to my own Scene

--- ShadeHeart2D/Assets/Scripts/Player/PlayerScript.cs
+++ ShadeHeart2D/Assets/Scripts/Player/PlayerScript.cs
@@ -1,23 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Animations;
-using UnityEngine.UIElements;
-
+using Unity.Mathematics;
 public class PlayerScript : MonoBehaviour
 {
+    // For Movement: 
     [SerializeField] private float walkingSpeed = 5f;
     private float sprintSpeed;
     private float currentMovementSpeed;
-    
-    private Rigidbody2D rb;
     private Vector2 movementDirection;
-    void Start()
+    // RigidBody 2D:
+    private Rigidbody2D rb;
+    // For Health:
+    private GameObject healthBarGameObject;
+    private Image healthBar;
+    [SerializeField] private float currentHealth = 100f;
+    private float maxHealth = 100;
+    // For Energy:
+    private GameObject energyBarGameObject;
+    private Image energyBar;
+    [SerializeField] private float currentEnergy = 50;
+    private float maxEnergy = 50;
+
+    private void Start()
     {
-            rb = GetComponent<Rigidbody2D>();
-            currentMovementSpeed = walkingSpeed;
+        // Set RigidBody:
+        rb = GetComponent<Rigidbody2D>();
+        //Get Health Bar Component:
+        healthBarGameObject = GameObject.FindWithTag("HealthBar");
+        healthBar = healthBarGameObject.GetComponent<Image>();
+        //Get Energy Bar Component:
+        energyBarGameObject = GameObject.FindWithTag("EnergyBar");
+        energyBar = energyBarGameObject.GetComponent<Image>();
+        //Set Variables:
+        currentMovementSpeed = walkingSpeed;
+        currentHealth =  maxHealth;
+        currentEnergy = maxEnergy;
     }
-    void updatingMovement()
+
+    private void Update()
+    {
+        updatingMovement();
+        updatingHealthAndEnergy();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = movementDirection * currentMovementSpeed;
+    }
+
+    // Updating Movement Speed: 
+    public void setWalkingSpeed(float newWalkingSpeed){walkingSpeed = newWalkingSpeed;}
+    public void setSprintSpeed(float newSprintSpeed){sprintSpeed = newSprintSpeed;}
+    private void updatingMovement()
     {
         
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
@@ -31,12 +67,9 @@ public class PlayerScript : MonoBehaviour
             currentMovementSpeed = walkingSpeed;
         }
     }
-    void Update()
+    private void updatingHealthAndEnergy()
     {
-        updatingMovement();
-    }
-    void FixedUpdate()
-    {
-        rb.velocity = movementDirection * currentMovementSpeed;
+        healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHealth, 0, 100);
+        energyBar.fillAmount = Mathf.Clamp(currentEnergy / maxEnergy, 0, 50);
     }
 }
