5774a421abb774dec94414c3c2de3cedad137733
Seth-Lopez <70123955+Seth-Lopez@users.noreply.github.com>
2023-12-04 22:21:44 -0800

    Fixed Glitch - Seth
    when merging ford's branch with main it cause some errors

---
+++ ShadeHeart2D/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+public class EnemyBehavior : MonoBehaviour
+{
+    // For Movement: 
+    [SerializeField] private float walkingSpeed = 1f;
+    private float sprintSpeed = 3f;
+    private bool walkPointSet = false;
+    private Vector3 destPoint;
+    private Vector3 previousPosition;
+    [SerializeField] private float rangePos = 3f;
+    [SerializeField] private float waitToMove = 5f;
+    [SerializeField] private float timer = 0f;
+    //NavMesh:
+    [SerializeField] LayerMask groundLayer;
+    UnityEngine.AI.NavMeshAgent agent;
+    // RigidBody 2D:
+    private Rigidbody2D rb;
+    // Line Of Sight
+    private GameObject player;
+    private bool hasLOS = false;
+    [SerializeField] private float LOSDist;
+    private void Start()
+    {
+        //Set RigidBody:
+        rb = GetComponent<Rigidbody2D>();
+        //Set Player Object:
+        player = GameObject.FindGameObjectWithTag("Player");
+        previousPosition = transform.position;
+        //Set NavMesh
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        updatingMovement();
+    }
+    private void FixedUpdate()
+    {
+        
+        lineOfSight();
+    }
+    
+    private void updatingMovement() 
+    { 
+        float x = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float y = Mathf.Abs(player.transform.position.y - transform.position.y);
+        timer += Time.deltaTime;
+        if(hasLOS && x <= LOSDist && y <= LOSDist)
+        {
+            agent.speed = sprintSpeed;
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            if(timer >= waitToMove)
+            {
+                agent.speed = sprintSpeed;
+                if(!walkPointSet) searchForDest();
+                if(walkPointSet) {agent.SetDestination(destPoint); }
+                if(Vector3.Distance(transform.position, destPoint) < 10f)
+                {
+                    walkPointSet = false;
+                    timer = 0;
+                }
+            }
+        }
+        
+    }
+    public void setWalkingSpeed(float newWalkingSpeed) { walkingSpeed = newWalkingSpeed; }
+    public void setSprintSpeed(float newSprintSpeed) { sprintSpeed = newSprintSpeed; }
+    public void setLineOfSightDistance(float newLOSDist) { LOSDist = newLOSDist; }
+    private void lineOfSight()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
+        if(ray.collider != null) hasLOS = ray.collider.CompareTag("Player");
+    }
+    private void searchForDest()
+    {
+        float x = UnityEngine.Random.Range(-rangePos, rangePos);
+        float y = UnityEngine.Random.Range(-rangePos, rangePos);
+        destPoint = new Vector3(transform.position.x + x, transform.position.y + y, 0f);
+        walkPointSet = true;
+    }
+    // Recursively set the opacity to 0 for the specified Transform and its children
+    void SetOpacityToZeroRecursive(Transform parent)
+    {
+        SpriteRenderer renderer = parent.GetComponent<SpriteRenderer>();
+        if (renderer != null)
+        {
+            Color color = renderer.color;
+            color.a = 0f; // Set alpha (opacity) to 0
+            renderer.color = color;
+        }
+        for (int i = 0; i < parent.childCount; i++) SetOpacityToZeroRecursive(parent.GetChild(i));       
+    }
+}
+
